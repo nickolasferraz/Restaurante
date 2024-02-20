3f8da0b25737afce4edcc78f10527d678ed76deb
Robson <142177762+robsthecreator@users.noreply.github.com>
2024-02-20 19:32:24 -0300

    criação de modal de confirmação + estilização

---
+++ Programacao/modal-confirm.js
@@ -0,0 +1,42 @@
+const confirmarCompra = (produto) => {
+
+  const modal = document.createElement('dialog');
+  modal.innerHTML = `
+    <div class="modal-container">
+      <h2>Confirmação de compra</h2>
+      <p>Você adicionou ${produto.nome} ao carrinho!</p>
+      <button id="confirmar-compra">Confirmar</button>
+    </div>
+  `;
+
+
+
+  document.body.appendChild(modal);
+
+  modal.showModal();
+
+  setTimeout(() => {
+    modal.close();
+  }, 2500); 
+
+
+  modal.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.id === 'confirmar-compra') {
+
+      modal.close();
+    }
+});
+};
+
+const botoesCarrinho = document.querySelectorAll('.card--produtos button');
+
+for (const botao of botoesCarrinho) {
+    botao.addEventListener('click', () => {
+        const produto = {
+            nome: "ItemTeste",
+        };
+        confirmarCompra(produto);
+    });
+}
